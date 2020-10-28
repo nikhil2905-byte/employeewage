5c3493bf55dbc9ea3d5d510b5ded2289dac2d7ce
unknown <nikhilnakirikanti27@gmail.com>
2020-10-28 12:56:02 +0530

    add : Employee Present Or Absent

--- empWage.java
+++ empWage.java
@@ -1,5 +1,15 @@
 class Empwage {
    public static void main(String[] args){
    System.out.println("Employee Wage Computation Problem");
-}
+		int present = 1;
+		int absent = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == present) {
+        	System.out.println("Employee is Present.");
+			}
+        else {
+        	System.out.println("Employee is Absent.");
+			}
+
+		}
 }
