5b71b42b36aff252153babf6bca2dd35efbc1d56
unknown <nikhilnakirikanti27@gmail.com>
2020-10-28 17:49:07 +0530

    add : Calculate Employee wage for multiple companies

--- empWage.java
+++ empWage.java
@@ -1,22 +1,32 @@
 public class empWage {
-	public static void main(String[] args){
-   System.out.println("Employee Wage Computation Problem");
-	reFactorUC7 obj = new reFactorUC7();
-	obj.EmpWageCalculateMethod();
-	}
+   public static void main(String[] args) {
+      empWageFormMultipleCompanies google = new empWageFormMultipleCompanies("Google" , 100 , 20 , 100);
+      empWageFormMultipleCompanies microsoft = new empWageFormMultipleCompanies("MicroSoft" , 100 , 20 , 100);
+      System.out.println("Total Empwage for " + google.companyName + ":" + google.calculateTotalEmpWage());
+      System.out.println("Total Empwage for " + microsoft.companyName + ":" + microsoft.calculateTotalEmpWage());
+   }
 }
 
-class reFactorUC7 {
-	void EmpWageCalculateMethod() {
-      final int fullTime = 1;
-      final int partTime=0;
-      final int empRate=20;
+class empWageFormMultipleCompanies {
+	final int fullTime = 1;
+   final int partTime=0;
+	final String companyName;
+	final int empRatePerHour;
+	final int workingDays;
+	final int maxHoursPerMonth;
+
+	public empWageFormMultipleCompanies(String companyName,int empRatePerHour,int workingDays,int maxHoursPerMonth) {
+		this.companyName=companyName;
+		this.empRatePerHour=empRatePerHour;
+		this.workingDays=workingDays;
+		this.maxHoursPerMonth=maxHoursPerMonth;
+		}
+	public int calculateTotalEmpWage() {
+		int empHrs=0;
 		int totalDays=0;
 		int totalHours=0;
-      int empHrs;
-      int empWage=0;
-		int totalWage=0;
-			while(totalDays<20 && totalHours<100) {
+			while(totalDays<workingDays && totalHours<maxHoursPerMonth) {
+			totalDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck)
 			{
@@ -31,14 +41,11 @@ class reFactorUC7 {
         default:
           System.out.println("Employee is absent");
           empHrs=0;
-		 break;
          }
-         empWage=empHrs*empRate;
-			totalWage=empWage+totalWage;
-			totalDays++;
-			totalHours=totalHours+empHrs;
-        }
-			System.out.println("Total Employee Wage:" +totalWage);
-        }
+         	totalHours += empHrs;
+			System.out.println("Day :" +totalDays+ " " +"Emphours:" +empHrs);
+		}
+		return totalHours * empRatePerHour;
+		}
 }
 
