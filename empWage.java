c63df3f88cb9c0c26f5980d96cb08c33f05e1b34
unknown <nikhilnakirikanti27@gmail.com>
2020-10-28 14:22:28 +0530

    add :  Employee wage by using Switch case

--- empWage.java
+++ empWage.java
@@ -1,26 +1,29 @@
 class Empwage {
    public static void main(String[] args){
    System.out.println("Employee Wage Computation Problem");
-		int fullTime = 1;
-		int partTime=2;
-		int wagePerHour=20;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		int empHrs=0;
-		int salaryPerDay=0;
-        if (empCheck == fullTime) {
-        	System.out.println("Full time Employee");
-			empHrs=8;
-			}
-        else if (empCheck == partTime){
-        	System.out.println("Part time Employee");
-			empHrs=4;
-			}
-			else  {
-			System.out.println("Employee is Absent");
-         empHrs=0;
-			}
-			salaryPerDay = (empHrs*wagePerHour);
+      final int fullTime = 1;
+      final int partTime=2;
+      final int wagePerHour=20;
+      int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+      int empHrs=0;
+      int salaryPerDay=0;
+      switch (empCheck)
+			{
+        case fullTime:
+          System.out.println("Full time Employee");
+          empHrs=8;
+          break;
+        case partTime:
+          System.out.println("Part time Employee");
+          empHrs=4;
+          break;
+        default:
+          System.out.println("Employee is Absent");
+          empHrs=0;
+         }
+         salaryPerDay = (empHrs*wagePerHour);
          System.out.println(salaryPerDay);
 
-		}
+      }
 }
+
