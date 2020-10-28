8b83d6a45fb33df8a68a6153fb3fdabab067f3a6
unknown <nikhilnakirikanti27@gmail.com>
2020-10-28 13:16:13 +0530

    add : Full Time Employee Wage

--- empWage.java
+++ empWage.java
@@ -3,12 +3,21 @@ class Empwage {
    System.out.println("Employee Wage Computation Problem");
 		int present = 1;
 		int absent = 0;
+		int wagePerHour=20;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
+		int empHrs=0;
+		int salaryPerDay=0;
         if (empCheck == present) {
-        	System.out.println("Employee is Present.");
+        	System.out.println("Employee is Present");
+			empHrs=8;
+        	salaryPerDay = (empHrs*wagePerHour);
+        	System.out.println(salaryPerDay);
 			}
         else {
-        	System.out.println("Employee is Absent.");
+        	System.out.println("Employee is Absent");
+			empHrs=0;
+         salaryPerDay = (empHrs*wagePerHour);
+         System.out.println(salaryPerDay);
 			}
 
 		}
